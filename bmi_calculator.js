69ea5586002dc8b07b3a49ce5e626c39a582509c
jehros <john.mayer198411@gmail.com>
2022-11-20 19:19:30 +0800

    Manual Testing for BMI Calculator

--- bmi_calculator.js
+++ bmi_calculator.js
@@ -1,16 +1,15 @@
 
 
-let kiloGram = 55;
-let heightlnCM= 163;
+let kiloGram = 55;                                // assigning a value to a kilogram variable
+let heightlnCM= 163;                              // assigning a value to a height in Centemeter
 
-function compute(k, h){
-    let convert = h / 100;
-    let result = (k /convert);
-    let finalresult = result / convert;
+function compute(k, h){                           // creating a function to hold an argument in the parameter
+    
+    let convert = h / 100;                         // converting the centemeter value to meter 
+    let result = (k /convert);                     // pass the value resuts to a results variable
+    let finalresult = result / convert;            // the result variable will be computed to a variable convert as formula for BMI and pass it to the final result variable
     return finalresult
 }
 
-
-
-    let totalBMI = compute(kiloGram, heightlnCM);
-console.log(` Your BMI results: ${totalBMI}`);
+    let totalBMI = compute(kiloGram, heightlnCM);   //called the function and assign it to a total BMI variable
+    console.log(`Your BMI results: ${totalBMI}`);   // display the total BMI results
